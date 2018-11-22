5fd2878ae01dc6ef257531a80d0706a8ff6d7774
chaulin <>
2018-11-22 20:30:13 +0800

    ok go

--- src/components/pass/OpenD.js
+++ src/components/pass/OpenD.js
@@ -5,34 +5,41 @@
  *
  */
 import React, {Component} from 'react';
-import {Col, Card, Row, Button, Avatar, Modal , message} from 'antd';
+import {Col, Card, Row, Button, Avatar, Modal, Select, Input} from 'antd';
+import {Radio} from 'antd';
+import { message } from 'antd';
+
 import BreadcrumbCustom from '../BreadcrumbCustom';
 import PhotoSwipe from "photoswipe";
 import PhotoswipeUIDefault from "photoswipe/dist/photoswipe-ui-default";
 import 'photoswipe/dist/photoswipe.css';
 import 'photoswipe/dist/default-skin/default-skin.css';
 
+
+const RadioGroup = Radio.Group;
 const {Meta} = Card;
+const Option = Select.Option;
+
+const options = [
+    {label: 'Apple', value: 'Apple'},
+    {label: 'Pear', value: 'Pear'},
+    {label: 'Orange', value: 'Orange'},
+];
+const optionsWithDisabled = [
+    {label: 'Apple', value: 'Apple'},
+    {label: 'Pear', value: 'Pear'},
+    {label: 'Orange', value: 'Orange', disabled: false},
+];
+
 
 class PassOpenD extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            date: new Date()
-            , userList: []
-            , visible: false
-            , recordData: {}
-            , gallery: null
-        };
-    }
 
 
     componentDidMount() {
 
-        console.log('hcia',this.props.match.params.id)
+        console.log('hcia', this.props.match.params.id)
 
-       var self = this
+        var self = this
 
         window.Axios.post('http://mobile.nooko.cn:8090/open/getOpenApplyDetail', {
             'id': self.props.match.params.id,
@@ -40,14 +47,13 @@ class PassOpenD extends Component {
             // 'token': this.props.match.params.id,
             'language': "zh-CN"
         }).then(function (response) {
-            console.log('hcia',response.data.data);
-            console.log('hcia',response.data.code);
+            console.log('hcia', response.data.code);
 
             self.setState({
                 recordData: response.data.data,
             });
 
-            console.log('hcia',self.state.recordData);
+            console.log('hcia', self.state.recordData);
 
         }).catch(function (error) {
             console.log(error);
@@ -64,7 +70,6 @@ class PassOpenD extends Component {
         var me = this;
 
         window.Axios.post('/open/passOpenApply', {
-            // "language":"zh-CN","userId":"#############################"
             'language': 'zh-CN',
             'id': me.state.recordData.id,
         }).then(function (response) {
@@ -75,9 +80,9 @@ class PassOpenD extends Component {
         });
 
 
-
     };
     saveData = () => {
+        message.success('ok 開戶成功')
         this.props.history.goBack()
     };
     saveReject = () => {
@@ -90,9 +95,9 @@ class PassOpenD extends Component {
             'language': 'zh-CN',
             'id': me.state.recordData.id
         }).then(function (response) {
-            console.log('hcia response',response);
+            console.log('hcia response', response);
 
-            console.log('hcia',response);
+            console.log('hcia', response);
 
         }).catch(function (error) {
             console.log(error);
@@ -142,61 +147,150 @@ class PassOpenD extends Component {
         if (!this.gallery) return;
         this.gallery.close();
     };
+    handleChange = (value) => {
+        console.log(value); // { key: "lucy", label: "Lucy (101)" }
+    };
 
+    state = {
+        value3: 'Apple',
+    }
+
+
+    onChange3 = (e) => {
+        console.log('radio3 checked', e.target.value);
+        this.setState({
+            value3: e.target.value,
+        });
+    }
+
+
+    onChangeSSS = (e) => {
+        console.log('radio3 checked', e.target.value);
+        this.setState({
+            sss: e.target.value,
+        });
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date()
+            , userList: []
+            , visible: false
+            , recordData: {}
+            , sss: 'aa'
+            , gallery: null
+        };
+    }
 
     render() {
 
-        const imgs = [
-            [
-                'http://mobile.nooko.cn:9080/MediaCenter/data/REGISTER/2c902bd8661e123f016676ea1fb10013/idcard_0-203906pgP2t.jpg',
-                'http://mobile.nooko.cn:9080/MediaCenter/data/REGISTER/2c902bd8661e123f016676ea1fb10013/idcard_0-203906pgP2t.jpg',
-                'http://mobile.nooko.cn:9080/MediaCenter/data/REGISTER/2c902bd8661e123f016676ea1fb10013/idcard_0-203906pgP2t.jpg',
-                'http://mobile.nooko.cn:9080/MediaCenter/data/REGISTER/2c902bd8661e123f016676ea1fb10013/idcard_0-203906pgP2t.jpg',
-                'http://mobile.nooko.cn:9080/MediaCenter/data/REGISTER/2c902bd8661e123f016676ea1fb10013/idcard_0-203906pgP2t.jpg',
-
-            ]
-        ];
-        const imgsTag = imgs.map(v1 => (
-            v1.map(v2 => (
-                <Row gutter={8}>
-                    <Col md={6}>
-                        <Card className="gutter-box" key={v2} bordered={true} bodyStyle={{padding: 0}}>
-                            <div>
-                                <img onClick={() => this.openGallery(v2)} alt="example" width="100%" src={v2}/>
-                            </div>
-                            <div className="pa-m">
-                                <h3>React Admin</h3>
-                                <small><a href="https://yezihaohao.github.io/" target="_blank"
-                                          rel="noopener noreferrer">https://yezihaohao.github.io/</a></small>
-                            </div>
-                        </Card>
-                    </Col>
-                </Row>
-            ))
-        ));
 
         return (
             <div>
 
                 <div>id: {this.state.recordData.id}</div>
-                <div>id: {this.state.recordData.id}</div>
                 <div>idcard_0 :{this.state.recordData.idcard_0}</div>
+                <div>sss :{this.state.sss}</div>
 
                 <BreadcrumbCustom first="审核管理" second="开户审核"/>
                 <Card title="IX账户审核 " bordered={true}>
 
-                    <Row gutter={8}>
-                        <Col md={6}>
-                            <Card title="客户姓名:{this.state.recordData.idv}" bordered={true}>
+                    <Row gutter={16}>
+                        <Col md={8}>
+                            <Card bordered={true}>
                                 <div>
 
-                                    <p>IX账户审核</p>
-                                    <p>IX账户审核</p>
+                                    <p>客户姓名:{this.state.recordData.cnName}</p>
+
                                 </div>
                             </Card>
                         </Col>
-                        <Col md={6}>
-                            <Card title="cssModule" bordered={true}>
+                        <Col md={8}>
+                            <Card bordered={true}>
+                                <div>
+
+                                    <p>客户邮箱：{this.state.recordData.email}</p>
+
+                                </div>
+                            </Card>
+                        </Col>
+                        <Col md={8}>
+                            <Card bordered={true}>
+                                <div>
+
+                                    <p>手机号码：{this.state.recordData.phoneNumber} </p>
+
+                                </div>
+                            </Card>
+                        </Col>
+
+
+                    </Row>
+                </Card>
+                <Card title="IX账户设置" bordered={true}>
+
+                    <Row gutter={16}>
+                        <Col md={24}>
+                            <Card bordered={true}>
+
+                                <div>
+                                    账户类型 :
+                                    <RadioGroup options={optionsWithDisabled} onChange={this.onChange3}
+                                                value={this.state.value3}/>
+                                </div>
+                            </Card>
+                            <Card bordered={true}>
+                                <div>
+                                    {/*<p>服务器</p>*/}
+                                    服务器 :
+                                    <Select labelInValue defaultValue={{key: 'lucy'}} style={{width: 120}}
+                                            onChange={this.handleChange}>
+                                        <Option value="jack">Jack (100)</Option>
+                                        <Option value="lucy">Lucy (101)</Option>
+                                    </Select>
+                                </div>
+
+                            </Card>
+
+                        </Col>
+
+                    </Row>
+                </Card>
+                <Card title="IX账户申请表单" bordered={true}>
+
+                    <Row gutter={8}>
+                        <Col md={12}>
+                            <Card title="基本信息" bordered={true}>
+                                <div>
+                                    <p>国家<Select  labelInValue defaultValue={{key: 'lucy'}} style={{marginLeft: 10 , width: 120}}
+                                                  onChange={this.handleChange}>
+                                        <Option value="jack">Jack (100)</Option>
+                                        <Option value="lucy">a (101)</Option>
+                                        <Option value="lucya">b (101)</Option>
+                                        <Option value="lucyb">Lubcy (101)</Option>
+                                        <Option value="lucyc">Lucdy (101)</Option>
+                                        <Option value="lucyd">Lucys (101)</Option>
+                                        <Option value="lucye">Lucy a(101)</Option>
+                                    </Select></p>
+                                </div>
+                                <div>
+                                    <p>*姓（中文）</p>
+                                </div>
+                                <div>
+                                    <p>IX账户审核<Input defaultValue={this.state.sss}  onChange={this.onChangeSSS} style={{marginLeft: 10, width: 120}} placeholder="Basic usage"/></p>
+                                </div>
+                            </Card>
+                            <Card bordered={true}>
+                                <div>
+
+                                    <p>*姓（中文）</p>
+                                </div>
+                            </Card>
+
+                        </Col>
+                        <Col md={12}>
+                            <Card title="资产&风险审核" bordered={true}>
                                 <div>
                                     <p>IX账户审核</p>
                                 </div>
@@ -208,34 +302,70 @@ class PassOpenD extends Component {
                                 />
                             </Card>
                         </Col>
-                        <Col md={6}>
+                    </Row>
+                </Card>
+                <Card title="IX账户身份信息" bordered={true}>
+                    <Row gutter={16}>
+
+                        <Col md={8}>
                             <Card className="gutter-box" bordered={true} bodyStyle={{padding: 0}}>
                                 <div>
                                     <img
-                                        onClick={() => this.openGallery('http://mobile.nooko.cn:9080/MediaCenter/data/REGISTER/2c902bd8661e123f016676ea1fb10013/idcard_0-203906pgP2t.jpg')}
+                                        onClick={() => this.openGallery(this.state.recordData.idcard_0)}
                                         alt="example" width="100%"
                                         src={this.state.recordData.idcard_0}/>
                                 </div>
                                 <div className="pa-m">
-                                    <h3>React Admin</h3>
-                                    <small><a href="https://yezihaohao.github.io/" target="_blank"
-                                              rel="noopener noreferrer">https://yezihaohao.github.io/</a></small>
+                                    <h3>身份证正面照片</h3>
+                                    <small><a href={this.state.recordData.idcard_0} target="_blank"
+                                              rel="noopener noreferrer">身份证正面照片</a></small>
+                                </div>
+                            </Card>
+                        </Col>
+                        <Col md={8}>
+                            <Card className="gutter-box" bordered={true} bodyStyle={{padding: 0}}>
+                                <div>
+                                    <img
+                                        onClick={() => this.openGallery(this.state.recordData.idcard_1)}
+                                        alt="example" width="100%"
+                                        src={this.state.recordData.idcard_1}/>
+                                </div>
+                                <div className="pa-m">
+                                    <h3>身份证反面照片</h3>
+                                    <small><a href={this.state.recordData.idcard_1} target="_blank"
+                                              rel="noopener noreferrer">身份证反面照片</a></small>
+                                </div>
+                            </Card>
+                        </Col>
+                        <Col md={8}>
+                            <Card className="gutter-box" bordered={true} bodyStyle={{padding: 0}}>
+                                <div>
+                                    <img
+                                        onClick={() => this.openGallery(this.state.recordData.idcard_2)}
+                                        alt="example" width="100%"
+                                        src={this.state.recordData.idcard_2}/>
+                                </div>
+                                <div className="pa-m">
+                                    <h3>身份证正面照片</h3>
+                                    <small><a href={this.state.recordData.idcard_2} target="_blank"
+                                              rel="noopener noreferrer">手持身份证照片</a></small>
                                 </div>
                             </Card>
                         </Col>
                     </Row>
-                </Card>
-                <Card title="IX账户审核" bordered={true}>
 
-                    <Row gutter={8}>
-                        <Col md={6}>
+                </Card>
+                <Card title="IX账户身份查重" bordered={true}>
+
+                    <Row gutter={12}>
+                        <Col md={12}>
                             <Card title="cssModule" bordered={true}>
                                 <div>
                                     <p>IX账户审核</p>
                                 </div>
                             </Card>
                         </Col>
-                        <Col md={6}>
+                        <Col md={12}>
                             <Card title="cssModule" bordered={true}>
                                 <div>
                                     <p>IX账户审核</p>
@@ -250,7 +380,7 @@ class PassOpenD extends Component {
                         </Col>
                     </Row>
                 </Card>
-                <Card title="IX账户审核" bordered={true}>
+                <Card title="IX账户审核备注" bordered={true}>
                     <div>
                         <Button onClick={() => this.openOK()}>开户通过</Button>
                         <Button onClick={() => this.saveData()}>保存</Button>
