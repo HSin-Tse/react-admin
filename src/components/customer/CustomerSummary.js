5a1ad673a8423b78bf9571c93fa8e5c11ac5eabe
002 <shihli.chen@infinox.cn>
2018-12-04 09:49:01 +0800

    edite  forzenAccount btn of  CustomerSummary

--- src/components/customer/CustomerSummary.js
+++ src/components/customer/CustomerSummary.js
@@ -296,7 +296,7 @@ requestUserCommentList = () =>{
 				width: 300,
 				render: (text, record) => (
 					<div>
-						<Button className="ant-dropdown-link">凍結</Button>
+						<Button className="ant-dropdown-link" onClick={ () => this.forzenAccount(record)}>凍結</Button>
 						<Button className="ant-dropdown-link">手機號解綁</Button>
 					</div>
 					),
@@ -310,7 +310,7 @@ requestUserCommentList = () =>{
 					<div>
 						{/* <span className="ant-divider" />
 						<Button className="ant-dropdown-link" onClick={() => this.handleremove(record)}>移除</Button> */}
-						<Button className="ant-dropdown-link" onClick={() => this.showModal()}>添加備註</Button>
+						<Button className="ant-dropdown-link" onClick={() => this.showModal(record)}>添加備註</Button>
 						<Button className="ant-dropdown-link" onClick={() => this.showModal2()}>操作日誌</Button>
 
 					</div>
@@ -353,6 +353,38 @@ requestUserCommentList = () =>{
 	}
 	goToUserAccountInfo = () => {
 		console.log('goToUserAccountInfo')
+	}
+	addComment = (e) =>{
+		let comment = e.target.value;
+		console.log('tttt',comment)
+		this.setState({
+			theComment: comment
+
+		});
+		
+		// this.setState({
+		// 	modal2Visible: true,
+		// 	visible: false,
+
+		// });
+
+	}
+	forzenAccount = (record) =>{
+		console.log('CustomerSummary record data',record);
+
+
+		let self = this
+		window.Axios.post('star/blockStarLiveAccount', {
+			"starClientAccount":record.accountNo,
+			"belongUserId":record.belongUserId,
+		
+		}).then((response) => {
+
+			console.log('CustomerSummary Axios sucessful', response);
+			alert(record.accountNo + '帳號凍結成功')
+		}).catch(function (error) {
+			console.log(error);
+		});
 	}
 
 	render() {
@@ -430,8 +462,10 @@ requestUserCommentList = () =>{
 					onCancel={this.handleCancel}
 					okText="確認"
 					cancelText="取消"
-				>
-					<p><Input placeholder="填写回访次数以及结果" /></p>
+					
+			>
+					<p><Input onChange={this.addComment} placeholder="填写回访次数以及结果" /></p>
+					
 				</Modal>
 				<Modal
 					title="操作日誌"
