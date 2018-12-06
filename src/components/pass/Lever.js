7da03ef3c2d023fded3b8f7cf4d3ee04b67449a7
chaulin <>
2018-12-06 20:23:03 +0800

    ui

--- src/components/pass/Lever.js
+++ src/components/pass/Lever.js
@@ -2,7 +2,7 @@
  * Created by tse on 2017/7/31.
  */
 import React, {Component} from 'react';
-import {message, Input, Button, Card, Table, Select, Modal, Popconfirm, Icon} from 'antd';
+import {message, Input, Button, Card, Table, Select, Modal, Popconfirm, Row, Col} from 'antd';
 import BreadcrumbCustom from '@/components/BreadcrumbCustom';
 import beauty from '@/style/imgs/beauty.jpg';
 import {bindActionCreators} from "redux";
@@ -24,6 +24,62 @@ class Basic extends Component {
             visibleB: false,
             user: '',
             userList: [],
+            leavgeList: [{
+                "id": 21,
+                "status": 1,
+                "clientGroupId": 404,
+                "leverage": 100,
+                "brokerCode": "BahamaIX",
+                "maxBalance": 500000.0,
+                "minBalance": 200001.0,
+                "maxLots": null,
+                "clientGroupName": "L_100",
+                "delFlag": 0
+            }, {
+                "id": 22,
+                "status": 1,
+                "clientGroupId": 406,
+                "leverage": 200,
+                "brokerCode": "BahamaIX",
+                "maxBalance": 200000.0,
+                "minBalance": 100001.0,
+                "maxLots": null,
+                "clientGroupName": "L_200",
+                "delFlag": 0
+            }, {
+                "id": 23,
+                "status": 1,
+                "clientGroupId": 405,
+                "leverage": 300,
+                "brokerCode": "BahamaIX",
+                "maxBalance": 100000.0,
+                "minBalance": 50001.0,
+                "maxLots": null,
+                "clientGroupName": "L_300",
+                "delFlag": 0
+            }, {
+                "id": 24,
+                "status": 1,
+                "clientGroupId": 304,
+                "leverage": 400,
+                "brokerCode": "BahamaIX",
+                "maxBalance": 50000.0,
+                "minBalance": 0.0,
+                "maxLots": null,
+                "clientGroupName": "L_400",
+                "delFlag": 0
+            }, {
+                "id": 25,
+                "status": 0,
+                "clientGroupId": 707,
+                "leverage": 50,
+                "brokerCode": "BahamaIX",
+                "maxBalance": 0.0,
+                "minBalance": 0.0,
+                "maxLots": null,
+                "clientGroupName": "L_50",
+                "delFlag": 0
+            }],
             detail: {
                 "name": null,
                 "id": "27",
@@ -83,6 +139,18 @@ class Basic extends Component {
     }
 
     componentDidMount() {
+
+        let self = this
+
+        window.Axios.post('dict/leverageList', {
+            'keys': 'IX_Income,IX_Percentage,IX_FundsSource,IX_UStax,IX_Trading_Experience,IX_Trading_Objectives,IX_Risk_Tolerance,open_type_ix,account_type',
+        }).then((response) => {
+            console.log('hcia response', response)
+            self.setState({
+                leavgeList: response.data.data,
+            })
+        });
+
         this.columns = [
             {
                 title: '手机号',
@@ -285,10 +353,15 @@ class Basic extends Component {
     }
 
     render() {
+        const gridStyle = {
+            width: '50%',
+            textAlign: 'center',
+        };
         return (
             <div>
                 {/*{JSON.stringify(this.props.todps)}*/}
                 <Modal
+                    width={500}
                     title={this.state.modeState == '正常' ? '恢复正常' : this.state.modeState}
                     onCancel={this.handleCancel}
                     visible={this.state.visibleB}
@@ -309,23 +382,42 @@ class Basic extends Component {
                         </Popconfirm>
                     ]}
                 >
-                    <Card bordered={false}
+                    <Card
+                        title={'账户：'+this.state.detail.accountNo}
+                        bordered={true}>
 
-                    >
-
-                        <h3>当前杠杆 :{this.state.detail.currentLeverage}</h3>
-                        <h3>余额 :{this.state.detail.cashBalance}</h3>
-                        <h3>杠杆修改 :{this.state.detail.targetLeverage}</h3>
-                        <h3>保证金占比:{this.state.detail.marginLevel}</h3>
                         <div>
-                            <h3>处理备注：</h3>
+                            <Row style={{marginTop: 20}}>
+                                <Col style={{textAlign: 'right'}} span={10}>当前杠杆:</Col>
+                                <Col style={{textAlign: 'center'}} span={14}>{this.state.detail.currentLeverage}</Col>
+                            </Row>
+                            <Row style={{marginTop: 20}}>
+                                <Col style={{textAlign: 'right'}} span={10}>余额:</Col>
+                                <Col style={{textAlign: 'center'}} span={14}>{this.state.detail.cashBalance}</Col>
+                            </Row>
 
-                            <TextArea value={this.state.detail.comment} onChange={this.changeNote} rows={4}>
-
-                        </TextArea>
+                            <Row style={{marginTop: 20}}>
+                                <Col style={{textAlign: 'right'}} span={10}>杠杆修改:</Col>
+                                <Col style={{textAlign: 'center'}} span={14}><Select
+                                    defaultValue={this.state.detail.targetLeverage}
+                                    style={{marginLeft: 0}}>
+                                    {this.state.leavgeList.map(ccty => <Option
+                                        key={ccty.leverage}>1:{ccty.leverage}</Option>)}
+                                </Select></Col>
+                            </Row>
+                            <Row style={{marginTop: 20}}>
+                                <Col style={{textAlign: 'right'}} span={10}>保证金占比:</Col>
+                                <Col style={{textAlign: 'center'}} span={14}>{this.state.detail.marginLevel}</Col>
+                            </Row>
+                            <Row style={{marginTop: 20}}>
+                            <Col span={24}>处理备注：</Col>
+                            <Col style={{marginTop: 20}} span={24}>
+                                <TextArea value={this.state.detail.comment}
+                                          onChange={this.changeNote}
+                                          rows={4}></TextArea>
+                            </Col>
+                        </Row>
                         </div>
-
-
                     </Card>
 
 
@@ -349,7 +441,7 @@ class Basic extends Component {
                     </Card>
                 </Modal>
 
-                <BreadcrumbCustom first="权限管理" second="杠杆审核"/>
+                <BreadcrumbCustom first="审核管理" second="杠杆审核"/>
                 {/*<Button onClick={() => this.seeDetail()}*/}
                 {/*>详情:{this.state.count}</Button>*/}
                 {/*<Button*/}
