f859e8768357c43bb6d8a25dad765a6cf85d87b3
chaulin <>
2018-12-29 16:43:54 +0800

    審核操作

--- src/components/pass/Lever.js
+++ src/components/pass/Lever.js
@@ -252,9 +252,9 @@ class Basic extends Component {
                 align: 'center',
                 render: (text, record) => (
                     <div>
-                        <Button onClick={() => this.showOPDAyModal2(record)}>操作日志</Button>
-                        <Button onClick={() => this.showModalB(record)}>审核</Button>
-                        <Button onClick={() => this.showModalA(record)}>查看</Button>
+                        <Button  onClick={() => this.showOPDAyModal2(record)}>操作日志</Button>
+                        <Button  style={{display:record.displayStatus=='审核通过'?'none':''}}  onClick={() => this.showModalB(record)}>审核</Button>
+                        <Button  style={{display:!record.displayStatus=='审核通过'?'none':''}} onClick={() => this.showModalA(record)}>查看</Button>
                     </div>
                 ),
             }];
