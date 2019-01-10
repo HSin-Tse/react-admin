8d2d0d59601771b7fadeae5d48092db5104e6342
chaulin <>
2019-01-10 20:04:41 +0800

    add cs a

---
+++ src/components/pms/model/speList.js
@@ -0,0 +1,16 @@
+
+
+export const speList = [
+    {key: 'DEPOSIT_MANNUAL_CREATE', name: '创建'},
+    {key: 'DEPOSIT_FINANCE_CHECK ', name: '财务审核'},
+    {key: 'DEPOSIT_DETAIL ', name: '入金完成(只读)'},
+    {key: 'WITHDRAW_SERVICE_CHECK ', name: '客维审核'},
+    {key: 'WITHDRAW_MASTER_CHECK ', name: '后台审核'},
+    {key: 'WITHDRAW_BANK_CHECK ', name: '银行放款'},
+    {key: 'WITHDRAW_DETAIL ', name: '出金完成(只读)'},
+
+
+]
+
+
+export default speList
