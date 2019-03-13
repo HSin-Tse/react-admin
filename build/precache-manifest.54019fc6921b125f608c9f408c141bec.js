5a091aac50b6518375683255980736560172c696
chaulin <>
2019-03-13 17:28:56 +0800

    1.权限设置處理 2.出金頁面權限綁定 3.已開戶限制 4.只使用返回参数accountTxnAmt。如果是入金(type=1)，accountTxnAmt放在入金栏位，出金栏位为空；如果是出金(type=2)，accountTxnAmt放在出金栏位，入金栏位为空

---
+++ build/precache-manifest.54019fc6921b125f608c9f408c141bec.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "afcce769d89c0cd41bc2",
+    "url": "./static/css/main.9157b155.chunk.css"
+  },
+  {
+    "revision": "afcce769d89c0cd41bc2",
+    "url": "./static/js/main.afcce769.chunk.js"
+  },
+  {
+    "revision": "dd6afee94d75dcf8f8c7",
+    "url": "./static/js/1.dd6afee9.chunk.js"
+  },
+  {
+    "revision": "c81a84a763b3de176090",
+    "url": "./static/css/2.36af1413.chunk.css"
+  },
+  {
+    "revision": "c81a84a763b3de176090",
+    "url": "./static/js/2.c81a84a7.chunk.js"
+  },
+  {
+    "revision": "60a7f81ac111c791e84b",
+    "url": "./static/css/3.b4daae7e.chunk.css"
+  },
+  {
+    "revision": "60a7f81ac111c791e84b",
+    "url": "./static/js/3.60a7f81a.chunk.js"
+  },
+  {
+    "revision": "7081af438c7180e433de",
+    "url": "./static/js/runtime~main.7081af43.js"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "./static/media/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "defb98583257610e959baa67ab0fa53b",
+    "url": "./static/media/beauty.defb9858.jpg"
+  },
+  {
+    "revision": "a4f3a6068f06d3d530700498bfa735a6",
+    "url": "./index.html"
+  }
+];
