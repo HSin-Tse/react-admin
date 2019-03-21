d9cd7fd7f58c82e86b4e3140795214687836346f
chaulin <>
2019-03-21 16:26:57 +0800

    cc

--- src/components/pms/OpLog.js
+++ src/components/pms/OpLog.js
@@ -113,13 +113,13 @@ export default class BlackList extends Component {
     }
 
     timestampToTime = (timestamp) => {
-        const dateObj = new Date(+timestamp) // ps, 必须是数字类型，不能是字符串, +运算符把字符串转化为数字，更兼容
-        const year = dateObj.getFullYear() // 获取年，
-        const month = dateObj.getMonth() + 1 // 获取月，必须要加1，因为月份是从0开始计算的
-        const date = dateObj.getDate() // 获取日，记得区分getDay()方法是获取星期几的。
-        const hours = this.pad(dateObj.getHours())  // 获取时, this.pad函数用来补0
-        const minutes = this.pad(dateObj.getMinutes()) // 获取分
-        const seconds = this.pad(dateObj.getSeconds()) // 获取秒
+        const dateObj = new Date(+timestamp)
+        const year = dateObj.getFullYear()
+        const month = dateObj.getMonth() + 1
+        const date = dateObj.getDate()
+        const hours = this.pad(dateObj.getHours())
+        const minutes = this.pad(dateObj.getMinutes())
+        const seconds = this.pad(dateObj.getSeconds()) 
         return year + '-' + month + '-' + date + ' ' + hours + ':' + minutes + ':' + seconds
     };
     pad = (str) => {
