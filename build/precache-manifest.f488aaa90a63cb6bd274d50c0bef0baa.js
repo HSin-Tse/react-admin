512deb48bc40938a99662fb4ccb4bec2f4c74fec
chaulin <>
2019-03-28 17:11:22 +0800

    1.1.2 優化代碼

---
+++ build/precache-manifest.f488aaa90a63cb6bd274d50c0bef0baa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c019e9edb1f95e1b3146",
+    "url": "./static/css/main.9157b155.chunk.css"
+  },
+  {
+    "revision": "c019e9edb1f95e1b3146",
+    "url": "./static/js/main.c019e9ed.chunk.js"
+  },
+  {
+    "revision": "be134093b1add8c2e767",
+    "url": "./static/js/1.be134093.chunk.js"
+  },
+  {
+    "revision": "1e0da17112c6ebb2d9cd",
+    "url": "./static/css/2.e30e8ca0.chunk.css"
+  },
+  {
+    "revision": "1e0da17112c6ebb2d9cd",
+    "url": "./static/js/2.1e0da171.chunk.js"
+  },
+  {
+    "revision": "5e296a9645ed52e28e95",
+    "url": "./static/css/3.b4daae7e.chunk.css"
+  },
+  {
+    "revision": "5e296a9645ed52e28e95",
+    "url": "./static/js/3.5e296a96.chunk.js"
+  },
+  {
+    "revision": "57bb691261303b26933c",
+    "url": "./static/js/runtime~main.57bb6912.js"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "./static/media/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "defb98583257610e959baa67ab0fa53b",
+    "url": "./static/media/beauty.defb9858.jpg"
+  },
+  {
+    "revision": "eba88f20d2adc9e2f8276383460a407b",
+    "url": "./index.html"
+  }
+];
